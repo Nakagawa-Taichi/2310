9bdae2bf14e917b9157295801117b4c3badcc1cb
koyama154 <143458494+koyama154@users.noreply.github.com>
2023-10-22 21:33:32 +0900

    （バックエンド）ユーザー一覧、経費修正

---
+++ attendanceManagementSystem/src/main/java/com/example/attendance/entity/ExpenseEntity.java
@@ -0,0 +1,62 @@
+package com.example.attendance.entity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.springframework.format.annotation.DateTimeFormat;
+
+import lombok.Data;
+
+/**
+ * 経費情報 Entity
+ */
+@Entity  
+@Data  
+@Table(name = "expense", schema = "public") 
+public class ExpenseEntity {
+
+	/**
+	 * 経費ID
+	 */
+	@Id 
+	@GeneratedValue(strategy = GenerationType.IDENTITY)  
+	@Column(name = "expense_id")  
+	private Long id; 
+	
+	/**
+	 * ユーザーID
+	 */
+	@Column(name = "user_id")  
+	private Long userId; 
+	
+	/**
+	 * 申請日
+	 */
+	@Column(name = "application_date")
+	private Date applicationDate;
+	
+	/**
+	 * 経費項目
+	 */
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+	@Column(name = "expense_category")
+	private String expenseCategory;
+	
+	/**
+	 * 経費金額
+	 */
+	@Column(name = "amount")
+	private int amount;
+	
+	/**
+	 * 備考
+	 */
+	@Column(name = "description")
+	private String description;
+}
