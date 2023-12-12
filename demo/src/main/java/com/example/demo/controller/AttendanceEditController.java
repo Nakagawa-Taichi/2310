2f1427e4b35d1658cd2f0570fd385a83fe964e2d
yg0913 <141751892+yg0913@users.noreply.github.com>
2023-12-12 23:21:24 +0900

    勤怠修正

---
+++ demo/src/main/java/com/example/demo/controller/AttendanceEditController.java
@@ -0,0 +1,99 @@
+package com.example.demo.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.example.demo.dto.AttendanceUpdateRequest;
+import com.example.demo.entity.AttendanceEntity;
+import com.example.demo.service.AttendanceEditService;
+
+public class AttendanceEditController {
+
+	@Autowired
+	private AttendanceEditService attendanceEditService;
+	// 勤怠修正フォームの表示
+	/**
+     * 勤怠情報詳細画面を表示
+     * @param id 表示するユーザーID
+     * @param model Model
+     * @return 経費情報詳細画面
+     */
+	
+    @GetMapping("/attend/{id}")
+    public String displayView(@PathVariable Integer id, Model model) {
+        AttendanceEntity user = attendanceEditService.findById(id);
+        model.addAttribute("attendeData", user);
+        return "user/correction";
+    }
+
+    /**
+     * 勤怠編集画面を表示
+     * @param id 表示する経費ID
+     * @param model Model
+     * @return 経費編集画面
+     */
+
+    @GetMapping("/attend/{id}/edit")
+    public String displayEdit(@PathVariable Integer id, Model model) {
+        AttendanceEntity user = attendanceEditService.findById(id);
+        AttendanceUpdateRequest attendanceUpdateRequest = new AttendanceUpdateRequest();
+        attendanceUpdateRequest.setAttendanceId(user.getAttendance_id());
+        attendanceUpdateRequest.setUserId(user.getUser_id());
+        attendanceUpdateRequest.setStatus(user.getStatus());
+        attendanceUpdateRequest.setAttendanceDate(user.getAttendance_date());
+        attendanceUpdateRequest.setStart_time(user.getStart_time());
+        attendanceUpdateRequest.setLeavingDate(user.getLeavingDate());
+        attendanceUpdateRequest.setEndTime(user.getEndTime());
+        attendanceUpdateRequest.setWorkingHours(user.getWorkingHours());
+        attendanceUpdateRequest.setBreakTime(user.getBreakTime());
+        attendanceUpdateRequest.setRemarks(user.getRemarks());
+        
+        model.addAttribute("attendanceUpdateRequest", attendanceUpdateRequest);
+        return "user/correction";
+    }
+    
+    /**
+     * 勤怠更新
+     * @param expenseRequest リクエストデータ
+     * @param model Model
+     * @return 経費一覧画面
+     */
+    @RequestMapping(value = "/attend/update", method = RequestMethod.POST)
+    public String correction(@Validated @ModelAttribute AttendanceUpdateRequest attendanceUpdateRequest, BindingResult result, Model model) {
+        if (result.hasErrors()) {
+            List<String> errorList = new ArrayList<String>();
+            for (ObjectError error : result.getAllErrors()) {
+              errorList.add(error.getDefaultMessage());
+            }
+            model.addAttribute("validationError", errorList);
+            return "user/correction";
+          }
+      // ユーザー情報の更新
+      attendanceEditService.update(attendanceUpdateRequest);
+      return "redirect:/attendance/AttendanceList"; //パスの変更
+    }
+    
+    /**
+     * 勤怠情報削除
+     * @param id 表示するユーザーID
+     * @param model Model
+     * @return 経費一覧画面
+     */
+    @GetMapping("/attend/{id}/delete")
+    public String delete(@PathVariable Integer id, Model model) {
+      // ユーザー情報の削除
+      attendanceEditService.delete(id);
+      return "redirect:/attendance/AttendanceList";
+    }
+}
---
+++ demo/src/main/java/com/example/demo/service/AttendanceEditService.java
@@ -0,0 +1,62 @@
+package com.example.demo.service;
+
+import java.text.SimpleDateFormat;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.example.demo.dto.AttendanceUpdateRequest;
+import com.example.demo.entity.AttendanceEntity;
+import com.example.demo.repository.AttendanceRepository;
+
+@Service
+@Transactional(rollbackFor = Exception.class)
+public class AttendanceEditService {
+
+	@Autowired
+    private AttendanceRepository attendanceRepository;
+	 /**
+	   * 経費情報 主キー検索
+	   * @return 検索結果
+	   */
+	  public AttendanceEntity findById(Integer id) {
+	    return attendanceRepository.findById(id).get();
+	  }
+	
+	  /**
+	   * 勤怠情報 更新
+	   * @param expense 勤怠情報
+	   */
+	  public void update(AttendanceUpdateRequest attendanceUpdateRequest) {
+		  AttendanceEntity attendance = findById(attendanceUpdateRequest.getAttendanceId());
+		    attendance.setUser_id(attendanceUpdateRequest.getUserId());
+		    attendance.setStatus(attendanceUpdateRequest.getStatus());
+		    attendance.setAttendance_date(attendanceUpdateRequest.getAttendanceDate());
+		    attendance.setStart_time(attendanceUpdateRequest.getStart_time());
+		    attendance.setLeavingDate(attendanceUpdateRequest.getLeavingDate());
+		    attendance.setEndTime(attendanceUpdateRequest.getEndTime());
+		    attendance.setWorkingHours(attendanceUpdateRequest.getWorkingHours());
+		    attendance.setBreakTime(attendanceUpdateRequest.getBreakTime());
+		    attendance.setRemarks(attendanceUpdateRequest.getRemarks());
+		    // StringからDateへの変換処理
+		    SimpleDateFormat sdFormat = new SimpleDateFormat("yyyy-MM-dd");
+//          try {
+//				Date date = sdFormat.parse(attendanceUpdateRequest.getAttendanceDate());
+//				attendance.setAttendance_date(date);
+//			} catch (ParseException e) {
+//				// TODO 自動生成された catch ブロック
+//				e.printStackTrace();
+//			}
+//		    attendance.setAttendance_date(new Date());
+		    attendanceRepository.save(attendance);
+		}
+	  /**
+	   * ユーザー情報 物理削除
+	   * @param id ユーザーID
+	   */
+	  public void delete(Integer id) {
+	    AttendanceEntity attendance = findById(id);
+	    attendanceRepository.delete(attendance);
+	  }
+}
